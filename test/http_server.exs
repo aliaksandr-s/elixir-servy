2f72ecf7958511b76360370b5760da7322fb2e82
Alex S <aliaksandr@lyte.com>
2023-07-27 10:40:27 +0200

    Update

--- test/http_server.exs
+++ test/http_server.exs
@@ -3,7 +3,7 @@ defmodule HttpServerTest do
 
   alias Servy.HttpServer
 
-  test "accepts a request on a socket and sends back a response" do
+  test "accepts a request on a socket and sends back a response (using httpc)" do
     spawn(HttpServer, :start, [4000])
 
     {:ok, response} = :httpc.request(:get, {'http://localhost:4000/wildthings', ''}, [], [])
@@ -12,5 +12,33 @@ defmodule HttpServerTest do
 
     assert status_code == 200
     assert body == "Bears, Lions, Tigers"
+  end
+
+  test "accepts a request on a socket and sends back a response (using httpoison)" do
+    spawn(HttpServer, :start, [4000])
+
+    parent = self()
+
+    max_concurrent_requests = 5
+
+    # Spawn the client processes
+    for _ <- 1..max_concurrent_requests do
+      spawn(fn ->
+        # Send the request
+        {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+
+        # Send the response back to the parent
+        send(parent, {:ok, response})
+      end)
+    end
+
+    # Await all {:handled, response} messages from spawned processes.
+    for _ <- 1..max_concurrent_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
   end
 end
